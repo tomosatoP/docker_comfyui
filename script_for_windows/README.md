977ad1373e004f8feab0b27c43fc9d0a27787082
tomosatoP <120862812+tomosatoP@users.noreply.github.com>
2023-08-10 16:56:16 +0900

    Add files via upload

---
+++ script_for_windows/README.md
@@ -0,0 +1,4 @@
+# スクリプトファイルの使い方
+
+## comfyui.ps1
+`comfyui.ps1` を `PowerShell で実行` で実行する。
---
+++ script_for_windows/comfyui.ps1
@@ -0,0 +1,6 @@
+# Launch ComfyUI
+
+$distribution = "Ubuntu_WebSD"
+$username     = "comfyui"
+
+wslg -d $distribution -u $username --cd ~ -- sh launch_comfyui.sh
